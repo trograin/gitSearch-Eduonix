c3d4818e27ca21254bb767998dc6268e5687497c
Boban Alempijevic <boban.alempijevic@gmail.com>
2016-11-02 21:43:06 +0200

    First commit

---
+++ app/services/github.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService{
+    private username:string;
+    private client_id = 'bdeeeca0971bc4c74f56';
+    private client_secret= '8fec3880e999fd0174f4b77f58b5f07b69dd969d';
+
+    constructor(private _http: Http) {
+        console.log('Github Service ready...');
+        this.username = 'trograin';
+    }
+
+    getUser(){
+        return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+            .map(res => res.json());
+    }
+
+    getRepos(){
+        return this._http.get('http://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
+            .map(res => res.json());
+    }
+
+    updateUser(username:string){
+        this.username = username;
+    }
+}
